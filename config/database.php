6a4818d390a091c9f1d55ccc70f7f29bf7bb7fd1
doctor-nasty <nick@jaiani.uk>
2023-01-24 23:41:21 +0400

    fix zipcodes table

--- config/database.php
+++ config/database.php
@@ -43,6 +43,14 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'sqlite2' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL_2'),
+            'database' => env('DB_DATABASE_2', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS_2', true),
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -61,26 +69,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
-            'mysql2' => [
-                'driver' => 'mysql',
-                'url' => env('DATABASE_URL'),
-                'host' => env('DB_HOST_2', '127.0.0.1'),
-                'port' => env('DB_PORT_2', '3306'),
-                'database' => env('DB_DATABASE_2', 'forge'),
-                'username' => env('DB_USERNAME_2', 'forge'),
-                'password' => env('DB_PASSWORD_2', ''),
-                'unix_socket' => env('DB_SOCKET', ''),
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-                'prefix' => '',
-                'prefix_indexes' => true,
-                'strict' => true,
-                'engine' => null,
-                'options' => extension_loaded('pdo_mysql') ? array_filter([
-                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                ]) : [],
-        ],
+            ],
 
         'pgsql' => [
             'driver' => 'pgsql',
