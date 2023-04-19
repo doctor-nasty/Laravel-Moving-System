76250f98ac95f80095b84212362a6b1bd10d5495
Nick Jaiani <nick@jaiani.uk>
2023-04-19 09:08:32 -0400

    fixes

--- resources/views/partials/headertext.blade.php
+++ resources/views/partials/headertext.blade.php
@@ -1,4 +1,5 @@
 <div class="center">
-    <h2 class="text-blue h2 fw-semibold mb-2">MovingWyze.com</h2>
-    <p class="text-blue">Everything You Need For Relocation</p>
+    <h2 class="text-blue h2 fw-semibold mb-2">Need moving services?</h2>
+    <h2 class="text-blue h2 fw-semibold mb-2">We're here to help!</h2>
+    <p class="text-blue">Get free quotes from top quality movers.</p>
 </div>
